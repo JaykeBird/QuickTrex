4d123f26c93bf0c9399d970f49f4504161623c60
Jacob Huempfner <jacobrh94@hotmail.com>
2019-10-20 15:04:15 -0500

    Converted project to new csproj format
    Converted project to the new csproj format used in Visual Studio 2017 and newer. The new format will allow me to easily create a .NET Core version of this app in the near future.

--- QuickTranslate/Properties/AssemblyInfo.cs
+++ QuickTranslate/Properties/AssemblyInfo.cs
@@ -4,18 +4,6 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("QuickTrex")]
-[assembly: AssemblyDescription("Quick Translation/Localization Extension")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Jacob R. Huempfner")]
-[assembly: AssemblyProduct("QuickTrex")]
-[assembly: AssemblyCopyright("Copyright © 2016-2019 Jacob R. Huempfner")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -39,19 +27,4 @@ using System.Windows;
                                               //(used if a resource is not found in the page, 
                                               // app, or any theme specific resource dictionaries)
 )]
-
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.2.0")]
-[assembly: AssemblyFileVersion("1.2.2.0")]
-[assembly: NeutralResourcesLanguage("en-US")]
 
